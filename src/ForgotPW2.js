d96832893942c2bb813bcb8e760f572a8f57fa51
Sagi Mines <sagi1236@gmail.com>
2022-07-26 13:48:58 +0300

    Finished building the basic structure of phase2 of forgot password

---
+++ src/ForgotPW2.js
@@ -0,0 +1,30 @@
+import { Card, Form, Button } from 'react-bootstrap';
+import FormInput from './FormInput';
+import './ForgotPW2.css';
+
+function ForgotPW2() {
+  return (
+    <div className="container forgotpw2-container">
+      <h1 className="forgotpw2-title">Password Recovery</h1>
+      <Card>
+        <Card.Body>
+          <Card.Title>Verification</Card.Title>
+          <Card.Text>Please enter the code before the time runs out.</Card.Text>
+          <Card.Text>*counter*</Card.Text>
+          <Form>
+            <FormInput type="text" placeholder="Enter the code" />
+            <Button className="forgotpw2-btn" type="submit">
+              Change password
+            </Button>
+          </Form>
+          <Card.Text className="resend-text">
+            If you have not recieved the code{' '}
+            <Card.Link className="resend-link">click here</Card.Link>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default ForgotPW2;
