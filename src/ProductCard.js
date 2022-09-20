ccd680a48827e43abc4b7b6ddf956bc1bbf99f6b
Sagi Mines <sagi1236@gmail.com>
2022-09-20 16:41:32 +0300

    Added all the server requests needed for the process

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,19 +1,59 @@
 import { Form, Card, Button, Row, Col } from 'react-bootstrap';
 import NumericInput from 'react-numeric-input';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './styles/ProductCard.css';
 import { UserContext } from './UserContext';
-import { faGreaterThanEqual } from '@fortawesome/free-solid-svg-icons';
-import { getReq, postReq } from './DAL/serverData';
+import { postReq } from './DAL/serverData';
 
 function ProductCard(props) {
   const { user, setUser } = useContext(UserContext);
-  const [checkClick, setCheckClick] = useState(true);
   const [productsAmount, setProductsAmount] = useState({});
+  const [checkClick, setCheckClick] = useState(true);
 
   const handleAmountChange = (valueAsNumber, valueAsString, input) => {
     productsAmount[input.name] = valueAsNumber;
     setProductsAmount({ ...productsAmount });
+  };
+
+  const handleCheckProductClick = () => {
+    // const id = Number(e.target.name);
+
+    // const foundCart = cartData.cartProducts.find(cart => cart.id === id);
+    if (!checkClick) {
+      props.currentProduct.checked = true;
+      //calculate the price with or without the discount
+      user.totalCartPrice =
+        user.totalCartPrice +
+        (props.currentProduct.discount
+          ? props.currentProduct.amount * props.currentProduct.unitPrice -
+            props.currentProduct.amount *
+              props.currentProduct.unitPrice *
+              (props.currentProduct.discount * 0.01)
+          : props.currentProduct.amount * props.currentProduct.unitPrice);
+      user.totalCartItems = user.totalCartItems + props.currentProduct.amount;
+    } else {
+      props.currentProduct.checked = false;
+      user.totalCartPrice =
+        user.totalCartPrice -
+        (props.currentProduct.discount
+          ? props.currentProduct.amount * props.currentProduct.unitPrice -
+            props.currentProduct.amount *
+              props.currentProduct.unitPrice *
+              (props.currentProduct.discount * 0.01)
+          : props.currentProduct.amount * props.currentProduct.unitPrice);
+      user.totalCartItems = user.totalCartItems - props.currentProduct.amount;
+    }
+    const foundUser = user.finalCart.find(
+      cart => cart.id === props.currentProduct.id
+    );
+    if (foundUser) {
+      foundUser.checked = props.currentProduct.checked;
+    }
+
+    console.log(checkClick);
+    console.log(user);
+    setUser({ ...user });
+    setCheckClick(!checkClick);
   };
 
   const handleBuyNow = e => {
@@ -23,7 +63,7 @@ function ProductCard(props) {
   const handleAddToWishlist = async e => {
     if (user) {
       const productId = Number(e.target.slot);
-      const reqBody = { userId: user, productId };
+      const reqBody = { userId: user.userId, productId };
       try {
         await postReq('wishlist', reqBody);
       } catch {
@@ -40,7 +80,7 @@ function ProductCard(props) {
       const amount = productsAmount[productId]
         ? productsAmount[e.target.value]
         : 1;
-      const reqBody = { userId: user, productId, amount };
+      const reqBody = { userId: user.userId, productId, amount };
       try {
         await postReq('cart', reqBody);
       } catch {
@@ -51,30 +91,58 @@ function ProductCard(props) {
     }
   };
 
+  useEffect(() => {
+    if (props.currentProduct && props.currentProduct.checked === undefined) {
+      props.currentProduct.checked = true;
+      setCheckClick(props.currentProduct.checked);
+    }
+
+    // remove this if when done with the purchase process
+    if (props.currentProduct) {
+      if (!user.finalCart) {
+        user.finalCart = [];
+        user.finalCart.push(props.currentProduct);
+      } else {
+        const found = user.finalCart.find(
+          cart => cart.id === props.currentProduct.id
+        );
+        if (!found) {
+          user.finalCart.push(props.currentProduct);
+        } else {
+          found.checked = props.currentProduct.checked;
+          console.log(user);
+        }
+      }
+    }
+    setUser({ ...user });
+    console.log(user);
+  }, []);
+
   return (
     <>
       <Card className="product-card">
         {props.page === 'cart' && (
           <Form.Check
+            name={props.currentProduct.id}
             className="choose-button"
             type="checkbox"
             id="default-checkbox"
-            checked={checkClick}
-            onClick={() => setCheckClick(!checkClick)}
+            defaultChecked={checkClick}
+            onClick={handleCheckProductClick}
           />
         )}
 
-        <Card.Img
+        {/* <Card.Img
           src={
             (props.product && props.product.image) ||
             'https://d3m9l0v76dty0.cloudfront.net/system/photos/7649724/large/2f4ab58b69e32e69c9ea56a346cf1271.jpg'
           }
-        />
+        /> */}
         <Card.Body>
-          <Card.Title>
+          {/* <Card.Title>
             {(props.product && props.product.productName) ||
               'Dewalt DCD999 Hammer Drill'}
-          </Card.Title>
+          </Card.Title> */}
           {props.page === 'wishlist' && (
             <Card.Text>
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -174,43 +242,134 @@ function ProductCard(props) {
             </section>
           )}
           {(props.page === 'cart' || props.page === 'review') && (
-            <section className="card-buttons row">
-              <Row>
-                <Col>
-                  <Card.Text>Price: 10$</Card.Text>
-                </Col>
+            <>
+              <Card.Img src={props.currentProduct.image} />
+              <Card.Title>{props.currentProduct.productName}</Card.Title>
+              <section className="card-buttons row">
+                <Row>
+                  {props.page === 'cart' && (
+                    <Card.Text className="price-section-category">
+                      {props.currentProduct.discount ? (
+                        <span>
+                          {`${
+                            props.currentProduct.unitPrice *
+                              props.currentProduct.amount -
+                            props.currentProduct.unitPrice *
+                              props.currentProduct.amount *
+                              (0.01 * props.currentProduct.discount)
+                          }$ `}
+                          <span className="old-price">
+                            {`${
+                              props.currentProduct.unitPrice *
+                              props.currentProduct.amount
+                            }$`}
+                          </span>
+                        </span>
+                      ) : (
+                        <span>
+                          {` ${
+                            props.currentProduct.unitPrice *
+                            props.currentProduct.amount
+                          }$`}
+                        </span>
+                      )}
+                      <span className="price-per-unit">{` (${
+                        props.currentProduct.discount
+                          ? props.currentProduct.unitPrice -
+                            props.currentProduct.unitPrice *
+                              (0.01 * props.currentProduct.discount)
+                          : props.currentProduct.unitPrice
+                      }$ for 1 unit)`}</span>
+                    </Card.Text>
+                  )}
+                </Row>
 
-                <Col className="amount">
-                  <NumericInput min={1} max={100} value={1} />
-                </Col>
-              </Row>
+                {props.page === 'cart' && (
+                  <>
+                    <Row>
+                      <Col className="amount">
+                        <NumericInput
+                          onChange={props.onAmountChange}
+                          min={1}
+                          max={props.currentProduct.unitsInStock}
+                          value={props.currentProduct.amount}
+                          name={props.currentProduct.id}
+                        />
+                      </Col>
+                      <Col className="stock-container">
+                        <Card.Text
+                          className={
+                            props.currentProduct.unitsInStock
+                              ? 'on-stock'
+                              : 'out-of-stock'
+                          }
+                        >
+                          {props.currentProduct.unitsInStock
+                            ? 'On Stock!'
+                            : 'Out of Stock'}
+                        </Card.Text>
+                      </Col>
+                    </Row>
 
-              <Row className="buttons-section">
-                <Col lg={6}>
-                  <Button>Delete</Button>
-                </Col>
-                <Col lg={6}>
-                  <Button>Move to Wishlist</Button>
-                </Col>
-              </Row>
-              <Row className="stock-container">
-                <Card.Text
-                  className={
-                    props.product
-                      ? props.product.unitsInStock
-                        ? 'on-stock'
-                        : 'out-of-stock'
-                      : 'on-stock'
-                  }
-                >
-                  {props.product
-                    ? props.product.unitsInStock
-                      ? 'On Stock!'
-                      : 'Out of Stock'
-                    : 'On Stock!'}
-                </Card.Text>
-              </Row>
-            </section>
+                    <Row className="buttons-section">
+                      <Col lg={6}>
+                        <Button
+                          onClick={props.onDeleteClick}
+                          name={props.currentProduct.id}
+                        >
+                          Delete
+                        </Button>
+                      </Col>
+                      <Col lg={6}>
+                        <Button>Move to Wishlist</Button>
+                      </Col>
+                    </Row>
+                  </>
+                )}
+                {props.page === 'review' && (
+                  <Row>
+                    <Col>
+                      <Card.Text className="order-review-product-units">{`Units: ${props.currentProduct.amount}`}</Card.Text>
+                    </Col>
+                    <Col>
+                      <Card.Text className="price-section-review">
+                        {props.currentProduct.discount ? (
+                          <span>
+                            {`${
+                              props.currentProduct.unitPrice *
+                                props.currentProduct.amount -
+                              props.currentProduct.unitPrice *
+                                props.currentProduct.amount *
+                                (0.01 * props.currentProduct.discount)
+                            }$ `}
+                            <span className="old-price">
+                              {`${
+                                props.currentProduct.unitPrice *
+                                props.currentProduct.amount
+                              }$`}
+                            </span>
+                          </span>
+                        ) : (
+                          <span>
+                            {` ${
+                              props.currentProduct.unitPrice *
+                              props.currentProduct.amount
+                            }$`}
+                          </span>
+                        )}
+                        <span className="price-per-unit">{` (${
+                          props.currentProduct.discount
+                            ? props.currentProduct.unitPrice -
+                              props.currentProduct.unitPrice *
+                                (0.01 * props.currentProduct.discount)
+                            : props.currentProduct.unitPrice
+                        }$ for 1 unit)`}</span>
+                      </Card.Text>
+                    </Col>
+                  </Row>
+                )}
+              </section>
+            </>
           )}
 
           {props.page === 'order-confirmation' && (
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,22 +1,157 @@
 import ProductCard from './ProductCard';
 import CheckoutCard from './CheckoutCard';
-import { Row, Col, Card, Button } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import './styles/ShoppingCart.css';
+import { UserContext } from './UserContext';
+import { getReq, deleteReq, patchReq } from './DAL/serverData';
+import { useContext, useEffect, useState } from 'react';
 
 function ShoppingCart() {
+  const { user, setUser } = useContext(UserContext);
+  let [cartData, setCartData] = useState(null);
+
+  // updates the UI when the user deletes an item from the cart
+  const handleDeleteClick = async e => {
+    const productId = e.target.name;
+    //updates the database
+    if (
+      await deleteReq(`cart?user-id=${user.userId}&product-id=${productId}`)
+    ) {
+      console.log('The item has been successfully deleted from the database.');
+      const filtered = cartData.cartProducts.filter(
+        product => product.id !== Number(e.target.name)
+      );
+      cartData.cartProducts = filtered;
+      cartData.totalAmount = cartData.cartProducts
+        .map(product => product.amount)
+        .reduce((a, b) => a + b, 0);
+      cartData.totalPrice = cartData.cartProducts
+        .map(product =>
+          product.discount
+            ? product.unitPrice * product.amount -
+              product.unitPrice * product.amount * (0.01 * product.discount)
+            : product.unitPrice * product.amount
+        )
+        .reduce((a, b) => a + b, 0);
+      setCartData({ ...cartData });
+      user.finalCart = cartData.cartProducts;
+      user.totalCartItems = cartData.totalAmount;
+      user.totalCartPrice = cartData.totalPrice;
+      setUser({ ...user });
+      console.log(user);
+    }
+  };
+
+  // updates the UI if the user change the amount of a product
+  const handleAmountChange = async (valueAsNumber, valueAsString, input) => {
+    const productId = input.name;
+    //updates the database
+    if (
+      await patchReq(
+        `cart?user-id=${user.userId}&product-id=${productId}&amount=${valueAsString}`
+      )
+    ) {
+      console.log('The item has been successfully updated in the database');
+    }
+
+    const found = cartData.cartProducts.find(
+      product => product.id === Number(input.name)
+    );
+    found.amount = valueAsNumber;
+    cartData.totalAmount = cartData.cartProducts
+      .map(product => product.amount)
+      .reduce((a, b) => a + b, 0);
+
+    cartData.totalPrice = cartData.cartProducts
+      .map(product =>
+        product.discount
+          ? product.unitPrice * product.amount -
+            product.unitPrice * product.amount * (0.01 * product.discount)
+          : product.unitPrice * product.amount
+      )
+      .reduce((a, b) => a + b, 0);
+    setCartData({ ...cartData });
+    user.finalCart = cartData.cartProducts;
+    user.totalCartItems = cartData.totalAmount;
+    user.totalCartPrice = cartData.totalPrice;
+    setUser({ ...user });
+  };
+
+  const setTheCart = async () => {
+    cartData = {};
+    cartData.cart = await getReq(`cart/${user.userId}`);
+    cartData.cartProducts = [];
+
+    for (const cart of cartData.cart) {
+      cartData.cartProducts.push(await getReq(`products/${cart.productId}`));
+    }
+
+    for (const product of cartData.cartProducts) {
+      const productImg = (await getReq(`product-images/${product.id}`))[0]
+        .imageSrc;
+      product.image = productImg;
+      const foundCart = cartData.cart.find(
+        cart => cart.productId === product.id
+      );
+      product.amount = foundCart.amount;
+    }
+
+    cartData.totalAmount = cartData.cartProducts
+      .map(product => product.amount)
+      .reduce((a, b) => a + b, 0);
+
+    cartData.totalPrice = cartData.cartProducts
+      .map(product =>
+        product.discount
+          ? product.unitPrice * product.amount -
+            product.unitPrice * product.amount * (0.01 * product.discount)
+          : product.unitPrice * product.amount
+      )
+      .reduce((a, b) => a + b, 0);
+
+    setCartData({ ...cartData });
+    user.finalCart = cartData.cartProducts;
+    user.totalCartItems = cartData.totalAmount;
+    user.totalCartPrice = cartData.totalPrice;
+
+    setUser({ ...user });
+  };
+
+  useEffect(() => {
+    setTheCart();
+  }, []);
+
   return (
-    <div className="container shopping-cart-container">
-      <h1 className="shopping-cart-title">Shopping Cart</h1>
-      <Row className="shopping-cart-data">
-        <Col md>
-          <ProductCard page="cart" />
-          <ProductCard page="cart" />
-        </Col>
-        <Col md>
-          <CheckoutCard page="cart" />
-        </Col>
-      </Row>
-    </div>
+    <>
+      {cartData && (
+        <div className="container shopping-cart-container">
+          <h1 className="shopping-cart-title">Shopping Cart</h1>
+          <Row className="shopping-cart-data">
+            <Col md>
+              {cartData.cartProducts.map((product, idx) => (
+                // the products in the cart
+                <ProductCard
+                  key={idx.toString()}
+                  page="cart"
+                  currentProduct={product}
+                  onAmountChange={handleAmountChange}
+                  onDeleteClick={handleDeleteClick}
+                />
+              ))}
+            </Col>
+            <Col md>
+              <CheckoutCard
+                page="cart"
+                cartSummary={{
+                  totalAmount: user.totalCartItems,
+                  totalPrice: user.totalCartPrice,
+                }}
+              />
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
   );
 }
 
