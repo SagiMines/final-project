8e305397606a5ed7695fe6f74dffa39c599b6794
Sagi Mines <sagi1236@gmail.com>
2022-11-17 16:45:07 +0200

    Check context

--- src/DAL/serverData.js
+++ src/DAL/serverData.js
@@ -90,6 +90,8 @@ export const getReq = async route => {
 
 export const getUserIdFromCookie = async () => {
   console.log('in cookie function');
+  console.log(Cookies.get('user_id', { domain: 'workshop-s.herokuapp.com' }));
+  console.log(Cookies.get('user_id'));
   if (Cookies.get('user_id')) {
     console.log('in cookie if');
     try {
